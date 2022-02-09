ae3220b23e88e2402eebe41453644f85ee03086e
Caio Ghinis <caio.ghinis1@gmail.com>
2022-02-09 11:14:10 -0300

    Primeiro commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '37999',
+                nome: 'Nelson Muntz',
+                partido: 'Republicano',
+                fotos: [
+                    {url: 'nelson.png', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '45321',
+                nome: 'Moe Szyslak',
+                partido: 'Democrata',
+                fotos: [
+                    {url: 'moe.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '36',
+                nome: 'Homer Simpson',
+                partido: 'Republicano',
+                vice: 'Bart Simpson',
+                fotos: [
+                    {url: 'homer.jpg', legenda: 'Prefeito'},
+                    {url: 'bart.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '88',
+                nome: 'Marge Simpson',
+                partido: 'Democrata',
+                vice: 'Lisa Simpson',
+                fotos: [
+                    {url: 'marge.jpg', legenda: 'Prefeito'},
+                    {url: 'lisa.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
